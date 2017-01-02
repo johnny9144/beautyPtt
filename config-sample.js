7f6f3aec074f2eac294052b611c35772da943971
Johnny Huang <johnny914425@gmail.com>
2017-01-03 00:31:57 +0800

    add config sample

---
+++ config-sample.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mongodb: "",
+  hostname: "",
+  port: '443',
+  bot: {
+    line: {
+      authorization: "",
+      url: "https://api.line.me/v2/bot/message/reply"
+    }
+  }
+};
