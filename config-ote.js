cc317d7536e85a9f1128efefaab4112459115207
Johnny Huang <johnny914425@gmail.com>
2017-01-03 00:22:25 +0800

    public

--- config-ote.js
+++ config-ote.js
@@ -1,10 +1,10 @@
 module.exports = {
-  mongodb: "mongodb://localhost/beauty",
-  hostname: "bowei.link",
+  mongodb: "",
+  hostname: "",
   port: '443',
   bot: {
     line: {
-      authorization: "y8ldq8+gdHpQN+0JWwNkD1qg1fBG3oSYZ/nveLTPW+4jmqR+SCoCqUhme0+X6uWweSHa0LOp/94fGsjpRWISelE0ZmfwW8rPSVZzznBMbJ2qlRVPhcaM9MNhw/3OnlhjPqPR7h3KlVU041hu+SKZDAdB04t89/1O/w1cDnyilFU=",
+      authorization: "",
       url: "https://api.line.me/v2/bot/message/reply"
     }
   }
